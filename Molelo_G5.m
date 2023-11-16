7e84ab1b1e518856c816acfe675ea4548d8f2904
Jose-Corvo-Lv99 <150535324+Jose-Corvo-Lv99@users.noreply.github.com>
2023-11-16 11:46:11 +0000

    Add files via upload

--- Molelo_G5.m
+++ Molelo_G5.m
@@ -30,11 +30,12 @@ jlr=jr+jl;
 mt=mr+ml;
 
 Area_l=xl*yl;
-Beta=0.5*ro_marte*Area_l;
+Beta=0.5*2.05*ro_marte*Area_l;
 D=Beta;
 
 %Definição dos eixos
 cm_t=(ml*(zr+zl/2)+mr*(zr/2))/mt;
+
 %O centro do eixo das coordenadas vai estar no centro de massa
 pz_cm=cm_t-zr;
 
@@ -51,7 +52,7 @@ p1=[0 pz_cm -yl/2
     -pz_cm 0 xl/2
     yl/2 -xl/2 0];
 Fp1=T1*[sin(20*pi/180); 0; cos(20*pi/180)];
-%n1;
+
 np1=p1*Fp1;
 
 %p2=[-xl/2 yl/2 -pz_cm];
@@ -59,7 +60,7 @@ p2=[0 pz_cm yl/2
     -pz_cm 0 xl/2
     -yl/2 -xl/2 0];
 Fp2=T2*[sin(20*pi/180); 0; cos(20*pi/180)];
-%n2;
+
 np2=p2*Fp2;
 
 %p3=[xl/2 -yl/2 -pz_cm];
@@ -67,7 +68,7 @@ p3=[0 pz_cm -yl/2
     -pz_cm 0 -xl/2
     yl/2 xl/2 0];
 Fp3=T3*[-sin(20*pi/180); 0; cos(20*pi/180)];
-%n3;
+
 np3=p3*Fp3;
 
 %p4=[xl/2 yl/2 -pz_cm];
@@ -75,8 +76,9 @@ p4=[0 pz_cm yl/2
     -pz_cm 0 -xl/2
     -yl/2 -xl/2 0];
 Fp4=T4*[-sin(20*pi/180); 0; cos(20*pi/180)];
-%n4;
+
 np4=p4*Fp4;
+
 
 fp=[Fp1 Fp2 Fp3 Fp4];
 T_m=Fp1+Fp2+Fp3+Fp4;
@@ -85,27 +87,27 @@ np=[np1; np2; np3; np4];
 
 zI = [0;0;1];
 
-% simulation parameters
+% Parametros de simulação
 nx = 12;
 ny = 4;
 x0 = zeros(nx,1);
 Dt = 0.1;
 t = 0:Dt:60;
 T=mt*g_marte;
-u_L=[0.2*mt*g_marte;0.1;0.1;0.1]*(t>=0);
-u_NL = [T;0;0;0]*ones(size(t))+u_L;
+u_NL=[T;0;0;0]*ones(size(t));
+
 
 C = [   eye(3)    , zeros(3)   , zeros(3) , zeros(3)
         zeros(1,3), zeros(1,3) , zI'      , zeros(1,3)  ];
 
-% simulate nonlinear system
+% simulasão do sistema não-linear
 Nsim = length(t);
 x = zeros(nx,Nsim);
 y = zeros(ny,Nsim);
 x(:,1) = x0;
 for k = 1:Nsim
     % prepare variables:
-    p=x(1:3,k);
+    p=x(1:3,k)
     v= x(4:6,k);
     lbd = x(7:9,k);
     omg  = x(10:12,k);
@@ -116,26 +118,28 @@ for k = 1:Nsim
     % compute state derivative:
     fa=4*pi*ro_marte*v.^2;
     p_dot=R*v;
-    R_dot=R*skew(omg);
     lbd_dot = Euler2Q(lbd)*omg;
     v_dot= -skew(omg)*v + g_marte*R'*zI - R*D*R'*v - 1/mt*(T_m+fa);
     om_dot = -inv(jlr)*skew(omg)*jlr*omg + inv(jlr)*np;
     
+    %R_dot=R*skew(omg); preferivel ser utilizado o lbd_dot
+    
     x_dot = [p_dot;v_dot;lbd_dot;om_dot];
 
      % integrate state
-    dx(:,k+1) = x(:,k) + Dt*x_dot;
+    x(:,k+1) = x(:,k) + Dt*x_dot;
 
     % compute current output:
     y(:,k) = C*x(:,k);
     
 end
-
-figure(90321);
-plot(t,y(1:2,:),'--',t,y(1:2,:),'-.');
-grid on;
-
-figure(90323);
+figure(1001);
 plot(t,u_NL);
 grid on;
+
+figure(1002)
+plot(t,y(3,:));
+
+
+
 
